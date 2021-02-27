423b485f08010d9c3eb20a539ac776209368935e
Smitha <cto@travelini.co>
2021-02-27 18:26:40 +0400

    Initial commit.

---
+++ GitExplorer/GitExplorer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitExplorer
+//
+//  Created by Mandha Smitha S on 27/02/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
