3ef045bc29da1224846ad1e3826bc9c36ecef403
MandhaSmitha <smandhasmithas61@gmail.com>
2021-02-28 08:23:51 +0400

    Network request update. Default values added.

--- GitExplorer/GitExplorer/NetworkManager/NetworkRequest/NetworkRequest.swift
+++ GitExplorer/GitExplorer/NetworkManager/NetworkRequest/NetworkRequest.swift
@@ -10,6 +10,7 @@ import Alamofire
  
 typealias Params = Parameters
 
+/// Defines the attributes of a network request
 protocol BaseNetworkRequest {
     var baseUrl: String { get }
     var endpoint: String { get }
@@ -17,5 +18,31 @@ protocol BaseNetworkRequest {
     var method: HttpMethod { get }
     var headers: [String: String] { get }
     var encoding: ParamEncoding { get }
-    var sampleDataString: String? { get }
+    var successResponseFileName: String? { get }
+    var failureResponseFileName: String? { get }
+}
+
+/// Extension to provide default values for some attributes of `BaseNetworkRequest`
+/// Conforming types can use the default values ot override to provide new values
+extension BaseNetworkRequest {
+    var baseUrl: String {
+        return "https://api.github.com/"
+    }
+    var headers: [String: String] {
+        return ["Accept": "application/vnd.github.v3+json"]
+    }
+    var encoding: ParamEncoding {
+        switch self.method {
+        case .get:
+            return .urlEncoding
+        default:
+            return .jsonEncoding
+        }
+    }
+    var successResponseFileName: String? {
+        return nil
+    }
+    var failureResponseFileName: String? {
+        return nil
+    }
 }
